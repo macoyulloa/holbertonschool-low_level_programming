b3ab01c0d78169753e9e32f3874f3cb475f567fd
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-07 21:16:09 +0000

    Uploading code point 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function_name - Short description, single line
+ * Description: Longer description of the function)?
+ * section header: Section description)
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
