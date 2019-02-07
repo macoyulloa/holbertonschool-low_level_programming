9fd053bd5cfc4775fb55aa43b16cff22f71b9ce9
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-07 22:28:49 +0000

    Uploading code point 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function_name - Short description, single line
+ * Description: Longer description of the function)?
+ * section header: Section description)
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 16; n++)
+	{
+	printf("%x", n);
+	}
+	printf("\n");
+	return (0);
+}
