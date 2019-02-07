b4323a44087614855e3421e0020d940e16cbcbf6
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-07 21:57:57 +0000

    Uploading code point 6 & 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function_name - Short description, single line
+ * Description: Longer description of the function)?
+ * section header: Section description)
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		char str[n];
+		putchar(str);
+	}
+	putchar('\n');
+	return (0);
+}
