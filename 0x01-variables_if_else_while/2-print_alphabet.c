bcaee3cd9ac02137ffacdaa2cf7eeb7ec937e53e
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-07 20:00:36 +0000

    Uploading code point 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function_name - Short description, single line
+ * Description: Longer description of the function)?
+ * section header: Section description)
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a' ; l <= 'z' ; l++)
+	{
+		putchar(l);
+	}
+	putchar("\n");
+	return (0);
+}
