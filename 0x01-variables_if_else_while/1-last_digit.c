9c00371b4c3cc9a6ff6cd0811306b2622ac02419
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-07 18:55:17 +0000

    Gramatical error correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	last = n % 10;
 	if (last > 5)
 	{
-		printf("Last digit of %d is %d and is grater than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
 	else if (last == 0)
 	{
