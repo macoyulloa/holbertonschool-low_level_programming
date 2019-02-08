6a5315dda288a182c6a04f58a5cb04183c781566
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-08 03:55:08 +0000

    Uploading point 2 advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function_name - Short description, single line
+ * Description: Longer description of the function)?
+ * section header: Section description)
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n, num, segDig, primDig, terDig;
+
+	for (n = 0; n < 800; n++)
+	{
+		num = n / 10;
+		segDig = num % 10;
+		primDig = n / 100;
+		terDig = n % 10;
+		if (segDig < terDig)
+		{
+			putchar(primDig + '0');
+			putchar(segDig + '0');
+			putchar(terDig + '0');
+			if (n < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
