68e4579ce90b22dfa68a541bef260045ad0090f7
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-11 18:42:57 +0000

    Uploading point one and other files & prototypes

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * function_name - Short description, single line
+ * Description: Longer description of the function)?
+ * section header: Section description)
+ *
+ * Return: always 0
+ */ 
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar(10);
+	return (0);
+}
