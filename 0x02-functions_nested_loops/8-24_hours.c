c6f3a06c518094e9e272aa37bb37447321d06ac1
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-12 15:43:42 +0000

    Code correction point 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * jack_bauer - funtion main of the indicated point
  *
@@ -9,7 +8,25 @@ void jack_bauer(void)
 {
 	int p, s, t, c;
 
-	for (p = 0; p < 3; p++)
+	for (p = 0; p <= 1; p++)
+	{
+		for (s = 0; s <= 9; s++)
+		{
+			for (t = 0; t < 6; t++)
+			{
+				for (c = 0; c <= 9; c++)
+				{
+					_putchar('0' + p);
+					_putchar('0' + s);
+					_putchar(58);
+					_putchar('0' + t);
+					_putchar('0' + c);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	for (p = 2; p < 3; p++)
 	{
 		for (s = 0; s < 4; s++)
 		{
@@ -17,7 +34,6 @@ void jack_bauer(void)
 			{
 				for (c = 0; c <= 9; c++)
 				{
-					if (p == 2 && s == 3 && t == 5 && c == 9)
 					_putchar('0' + p);
 					_putchar('0' + s);
 					_putchar(58);
