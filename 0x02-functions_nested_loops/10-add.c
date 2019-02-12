a7b1f00e20af0eed4978b6eea1e474a114493455
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-12 18:21:45 +0000

    Uploading point 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_sign - funtion main of the indicated point
+ * @n: is the input
+ *
+ * Return: always return 0.
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
