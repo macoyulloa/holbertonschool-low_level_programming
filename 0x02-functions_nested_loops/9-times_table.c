477bbe5f3fb22242612860a1e1b69ab74259323d
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-12 23:26:17 +0000

    Correct format code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,17 +16,26 @@ void times_table(void)
 			t = i * b;
 			if (t <= 9)
 			{
-				_putchar(' ');
-				_putchar(' ');
+				if (i > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar(t + '0');
-				_putchar(',');
+				if (i != 9)
+				{
+					_putchar(',');
+				}
 			}
 			else
 			{
 				_putchar(' ');
 				_putchar(t / 10 + '0');
 				_putchar(t % 10 + '0');
-				_putchar(',');
+				if (i != 9)
+				{
+					_putchar(',');
+				}
 			}
 		}
 		_putchar('\n');
