5bdddbfcfbd177277c7b22b839f06e9030d05451
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-12 21:36:12 +0000

    sintax correction code 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,33 +11,33 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n; n <= 98 ; n++)
+		for (n = n; n <= 98 ; ++n)
 		{
 			if (n < 98)
 			{
 				printf("%d, ", n);
 			}
-			if (n == 98)
+			else if (n == 98)
 			{
 				printf("%d", n);
 			}
 		}
-		printf("\n");
+	printf("\n");
 	}
-	if (n > 98)
+	else if (n > 98)
 	{
 		for (n = n; n >= 98 ; n--)
 		{
 			if (n > 98)
 			{
-				printf("%d, ", n);
+			printf("%d, ", n);
 			}
-			if (n == 98)
+			else if (n == 98)
 			{
-				printf(" %d", n);
+				printf("%d", n);
 			}
 		}
-		printf("\n");
+	printf("\n");
 	}
 	if (n == 98)
 	{
