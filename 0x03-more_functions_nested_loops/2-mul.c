032a679d79bf11816db1d3b26c6411645858ef5a
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-14 19:49:22 +0000

    uploading code point 2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - exercises uses the loops
+ * @a: input
+ * @b: input
+ * Return: always 0.
+ */
+
+int mul(int a, int b)
+{
+	int total;
+
+	total = a * b;
+	return (total);
+}
