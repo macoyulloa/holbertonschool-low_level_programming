da7f566779c16c2f7bcf35d7c3ebd89d27d1691e
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-19 03:45:57 +0000

    Uploading code point 0 to 1 & the README.md

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: pointer
+ * @b: pointer
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
