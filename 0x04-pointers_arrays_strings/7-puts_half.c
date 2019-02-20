aa0a5f7915f88f69fdbbdc240e4fbf4d8de0df6a
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-20 02:58:50 +0000

    code

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * puts_half - code for Holberton School students.
+ * @str: pointer
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int length, n;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+	if (str[length] % 2 != 0)
+	{
+		for (n = ((length + 1) / 2); str[n] != '\0'; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		for (n = (length / 2); str[n] != '\0'; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar ('\n');
+}
