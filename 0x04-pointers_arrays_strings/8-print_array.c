579ecc06a47008dd3ca834888d69790ad6778d5e
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-20 04:04:27 +0000

    code

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,7 @@ void print_array(int *a, int n)
 {
 	int count;
 
-	for (count = 0; count != n; count++)
+	for (count = 0; count < n; count++)
 	{
 		if (count == (n - 1))
 			printf("%d", a[count]);
