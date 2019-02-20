4eccf7b63d7ca534e8814abd9fd53b6a536bb976
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-20 04:56:46 +0000

    code

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - code for Holberton School students.
+ * @dest: pointer
+ * @src: pointer
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
