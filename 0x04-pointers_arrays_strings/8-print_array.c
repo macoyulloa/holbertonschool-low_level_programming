92840f8b426807810752a319dddf8fa4d8d8d054
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-20 03:45:23 +0000

    code

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,9 @@ void print_array(int *a, int n)
 {
 	int c;
 
-	for(c = 0; c != n; c++)
+	for (c = 0; c != n; c++)
 	{
-		if (c < n-1)
+		if (c < n - 1)
 			printf("%d, ", a[c]);
 		else
 			printf("%d", a[c]);
