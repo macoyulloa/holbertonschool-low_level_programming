66b5e8d290c49e674ca6d5b7768cc71a58a8d42e
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-21 21:14:30 +0000

    code uploading

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -24,8 +24,8 @@ char *_strncat(char *dest, char *src, int n)
 		*destino = *fuente;
 		destino++;
 		fuente++;
-		if (i < n)
-		*destino = '\0';
 	}
+	if (i < n)
+	*destino = '\0';
 	return (dest);
 }
