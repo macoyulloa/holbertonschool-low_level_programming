3b54fddea363f74d7d0dab89bea23084c9c3cab6
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-21 21:02:26 +0000

    code uploading

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,8 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	/**char *destino;
-	char *fuente;**/
 	int i = 0;
 
-	/**destino = dest;
-	fuente = src;**/
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
