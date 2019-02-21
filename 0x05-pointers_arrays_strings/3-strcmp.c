52f1e654851dfbc71fb91a952b1477afcaabe17a
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-21 22:03:25 +0000

    Code uploading point 3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strncmp - check the code for Holberton School students.
+ * _strcmp - check the code for Holberton School students.
  * @s1: pointer
  * @s2: pointer
  * Return: Always 0.
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		r = s1[i] - s2[i];
-		if ( r == 0)
+		if (r == 0)
 		i++;
 		else
 		return (r);
