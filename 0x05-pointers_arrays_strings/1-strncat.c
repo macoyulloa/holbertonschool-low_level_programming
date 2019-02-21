a87702deec0671178f65ca13b2111a9d9275d4e7
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-21 18:32:00 +0000

    Uploading code point 0 & 1

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncat - check the code for Holberton School students.
+ * @dest: pointer
+ * @src: pointer
+ * @n: input
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *destino;
+	char *fuente;
+	int i;
+
+	destino = dest;
+	fuente = src;
+	for (; *destino != '\0'; destino++)
+	{
+	}
+	for (i = 0; fuente[i] != '\0'; i++)
+	{
+		if (i < n)
+		{
+			*destino = *fuente;
+			destino++;
+			fuente++;
+		}
+	}
+	return (dest);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory you could find the second exercises in order to learn about the uses of the pointers, arrays and strings in C programming. 
