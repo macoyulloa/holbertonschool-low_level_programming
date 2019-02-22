28553b08dff1befc606f7d1fa5bded08d9ba36ad
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-22 00:09:48 +0000

    Code point 4

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - check the code for Holberton School students.
+ * @a: pointer
+ * @n: input
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, count;
+	int n2 = n - 1;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		count = a[i];
+		a[i] = a[n2 - i];
+		a[n2 - i] = count;
+	}
+}
