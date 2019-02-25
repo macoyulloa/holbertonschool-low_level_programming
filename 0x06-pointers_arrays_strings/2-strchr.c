8072ab761919212eb208eededc4fe60778c6cde3
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-25 21:12:13 +0000

    Uploading code point 2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - funtion.
+ * @s: pointer
+ * @c: input
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int i, caracter;
+
+	for (i = 0; s[0] != '\0'; i++, s++)
+	{
+		for (caracter = 32; caracter != s[0]; caracter++)
+		{
+		}
+	if (caracter == c)
+		return (s);
+	}
+	return (0);
+}
