a93eddeea76d7cb60a139dbc83d627d1202df1b3
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-26 01:58:07 +0000

    Code point 4

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - funtion.
+ * @s: pointer
+ * @accept: pointer
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (c = 0; accept[c] != '\0'; c++)
+		{
+			if (accept[c] == s[i])
+			{
+				return (s+i);
+			}
+		}
+	}
+	return (0);
+}
