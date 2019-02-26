c165397ce3acf2e3bcdc6a2c8f96d91eb7e01fc8
Maria Alejandra Coy <malejacoy@hotmail.com>
2019-02-26 03:19:33 +0000

    Code point 8

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - funtion.
+ * @s: pointer
+ * @to: pointer
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
