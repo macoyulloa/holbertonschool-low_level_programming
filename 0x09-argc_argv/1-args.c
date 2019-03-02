79335da44471ac830098ee8b7e69a7c73f0ddae4
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-02 20:51:02 +0000

    uploading code point 1

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - function.
+ * @argc: input
+ * @argv: array
+ * Return: int
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
