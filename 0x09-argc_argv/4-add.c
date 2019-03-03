c0d2be2f2ce41222a051e9e6acca14a104437029
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-03 17:04:56 +0000

    Code correcion

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - function.
@@ -10,20 +11,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 	int sum = 0;
+	char *p;
 
-	if (argc > 1)
+	if (argc > 0)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			sum = sum + atoi(argv[i]);
+		p = argv[i];
+			for (j = 0; p[j] != '\0'; j++)
+			{
+				if (isalpha(p[j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		sum = sum + atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}
 	else
-	{
 		printf("0\n");
-	}
 	return (0);
 }
