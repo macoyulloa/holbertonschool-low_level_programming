53618392510f8c571a4fb1138d72b68a04b50aa6
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-05 20:49:50 +0000

    uploading code point 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -15,23 +15,26 @@ int **alloc_grid(int width, int height)
 
 	if (width  <= 0 || height <= 0)
 		return (NULL);
-	arr = (int **)malloc(width * sizeof(int *));
-	for (i = 0; i < width; i++)
-		arr[i] = (int *)malloc(height * sizeof(int));
-		if (arr == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			for (i = 0; i < width; i++)
+	else
+	{
+		arr = (int **)malloc(width * sizeof(int *));
+		for (i = 0; i < width; i++)
+			arr[i] = (int *)malloc(height * sizeof(int));
+			if (arr == NULL)
 			{
-				for (j = 0; j < height; j++)
-				{
-					arr[i][j] = 0;
-				}
+				return (NULL);
 			}
-		return (arr);
-		}
+			else
+			{
+				for (i = 0; i < width; i++)
+				{
+					for (j = 0; j < height; j++)
+					{
+						arr[i][j] = 0;
+					}
+				}
+				return (arr);
+			}
+	}
 	return (NULL);
 }
