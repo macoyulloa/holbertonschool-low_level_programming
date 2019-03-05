96df5d9e2ec47f763e990677999e00dfb6b9bae4
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-05 20:43:46 +0000

    uploading code point 3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - 2 dimensional array of integers
+ * @width: int
+ * @height: int
+ *
+ * Return: a 2 dimensional array.
+ **/
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width  <= 0 || height <= 0)
+		return (NULL);
+	arr = (int **)malloc(width * sizeof(int *));
+	for (i = 0; i < width; i++)
+		arr[i] = (int *)malloc(height * sizeof(int));
+		if (arr == NULL)
+			return (NULL);
+		else
+		{
+			for (i = 0; i < width; i++)
+			{
+				for (j = 0; j < height; j++)
+				{
+					arr[i][j] = 0;
+				}
+			}
+		return (arr);
+		}
+	return (NULL);
+}
