d64323cf3f214400727dbed7c2aa39fda71ae99e
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-07 22:20:06 +0000

    croquis

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: void pointer.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	return (arr);
+}
---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - show all the values  from min to max
+ * @min: minimo
+ * @max: maximo
+ *
+ * Return: void pointer.
+ */
+int *array_range(int min, int max)
+{
+	void *arr;
+
+	if (min > max)
+		return (NULL);
+	return (arr);
+}
