311b3f65a4d266e384b70dece1a1ceaf0171f034
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-07 20:15:59 +0000

    uploading code point 1

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function concatenar
+ * @s1: pointer
+ * @s2: pointer
+ * @n: input
+ * Return: depends on the case.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int leng1, leng2, count;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (leng1 = 0; s1[leng1] != '\0'; leng1++)
+	{
+	}
+	for (leng2 = 0; s2[leng2] != '\0'; leng2++)
+	{
+		leng2 = leng2 + 1;
+	}
+	if (leng2 < n)
+		leng2 = leng2;
+	else
+		leng2 = n;
+	s = malloc(sizeof(char) * (leng1 + leng2));
+	if (s == NULL)
+		return (NULL);
+	for (leng1 = 0; s1[leng1] != '\0'; leng1++)
+		s[leng1] = s1[leng1];
+	for (count = 0; count <= leng2; count++)
+		s[count + leng1] = s2[count];
+	s[leng1 + leng2] = '\0';
+	return (s);
+}
