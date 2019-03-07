0590d6013ef721167ca3642275dec4b23c30befa
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-07 21:02:29 +0000

    uploading code 1

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -19,23 +19,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	for (leng1 = 0; s1[leng1] != '\0'; leng1++)
-	{
-	}
+	;
 	for (leng2 = 0; s2[leng2] != '\0'; leng2++)
-	{
-		leng2 = leng2 + 1;
-	}
+	;
 	if (leng2 < n)
-		leng2 = leng2;
+		count = leng2;
 	else
-		leng2 = n;
-	s = malloc(sizeof(char) * (leng1 + leng2));
+		count = n;
+	s = malloc(sizeof(char) * (leng1 + count) + 1);
 	if (s == NULL)
 		return (NULL);
 	for (leng1 = 0; s1[leng1] != '\0'; leng1++)
 		s[leng1] = s1[leng1];
-	for (count = 0; count <= leng2; count++)
-		s[count + leng1] = s2[count];
-	s[leng1 + leng2] = '\0';
+	for (leng2 = 0; leng2 <= count; leng2++)
+		s[leng2 + leng1] = s2[leng2];
 	return (s);
 }
