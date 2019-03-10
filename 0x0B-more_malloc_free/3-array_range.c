7b03bf18cb729016c9406e4155bdd5548bdf1141
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-10 04:01:19 +0000

    uploading code point 3

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -12,12 +12,18 @@
  */
 int *array_range(int min, int max)
 {
-	void *arr;
+	int *arr;
+	int sum = 0, i;
 
 	if (min > max)
 		return (NULL);
-	arr = (malloc(sizeof(int) * (max - min)));
+	arr = (malloc(sizeof(int) * (max - min + 1)));
 	if (arr == NULL)
+	{
 		return (NULL);
+		free(arr);
+	}
+	for (i = 0; i <= max; min++, i++)
+		arr[i] = sum + min;
 	return (arr);
 }
