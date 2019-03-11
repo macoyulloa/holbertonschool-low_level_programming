2ace9c45351fa1173a221cac9f5bf3b347980d79
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-11 23:01:32 +0000

    uploading code point 1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - structure.
+ * @d: struct dog
+ * @name: string input
+ * @age: number input
+ * @owner: number input
+ * Retrun: void.
+**/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
