c3943b106853760b4144d6d1ada79134f52b4323
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-11 18:37:33 +0000

    uploading code point 0 & README.md

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,1 @@
+In this directory you could find the steps and excercises in order to understand and practice the structures and 3the typedef
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG1
+#define DOG1
+/**
+ * struct dog - structure.
+ *
+ * @name: string input
+ * @age: string input
+ * @owner: string input
+**/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
