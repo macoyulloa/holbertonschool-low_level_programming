8cc6c2acc340bd62459f75058c3abe5785862bed
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-12 19:13:38 +0000

    code correction

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -4,7 +4,6 @@
 /**
  * contar - cuenta los string.
  * @str: input
- *
  * Return: void.
 **/
 int contar(char *str)
@@ -15,7 +14,31 @@ int contar(char *str)
 	;
 	return (i + 1);
 }
+/**
+ * strcopy - cuenta los string.
+ * @str: input
+ * @ancho: ancho
+ * Return: void.
+**/
+char *strcopy(char *str, int ancho)
+{
+	int leng;
+	char *str1;
 
+	str1 = malloc(sizeof(char) * ancho);
+	if (str1 == NULL)
+	{
+		free(str1);
+		return (NULL);
+	}
+	else
+	{
+		for (leng = 0; str[leng] != '\0'; leng++)
+			str1[leng] = str[leng];
+		return (str1);
+	}
+	return (NULL);
+}
 /**
  * new_dog - structure.
  * @name: input
@@ -29,28 +52,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *nombre;
 	char *dueno;
 
-	nombre = name;
-	dueno = owner;
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+		return (NULL);
+	d->name = malloc(sizeof(char) * contar(name));
+	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	d->name = malloc(sizeof(char) * contar(name));
-	if (d->name == NULL)
-	{
-		free(d->name);
-		return (NULL);
-	}
+	nombre = strcopy(name, contar(name));
 	d->name = nombre;
 	d->age = age;
 	d->owner = malloc(sizeof(char) * contar(owner));
 	if (d->owner == NULL)
 	{
-		free(d->owner);
+		free(d);
+		free(d->name);
 		return (NULL);
 	}
+	dueno = strcopy(owner, contar(owner));
 	d->owner = dueno;
 	return (d);
 }
