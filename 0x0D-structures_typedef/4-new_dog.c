47e86c7afe69254ab51313308b40376aacfab3be
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-12 15:55:52 +0000

    code correction

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,20 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * contar - cuenta los string.
+ * @str: input
+ *
+ * Return: void.
+**/
+int contar(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	;
+	return (i + 1);
+}
 
 /**
  * new_dog - structure.
@@ -12,18 +26,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *nombre;
+	char *dueno;
 
+	nombre = name;
+	dueno = owner;
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = malloc(sizeof(char *));
+	d->name = malloc(sizeof(char) * contar(name));
 	if (d->name == NULL)
 		return (NULL);
-	d->name = name;
+	d->name = nombre;
 	d->age = age;
-	d->owner = malloc(sizeof(char *));
+	d->owner = malloc(sizeof(char) * contar(owner));
 	if (d->owner == NULL)
 		return (NULL);
-	d->owner = owner;
+	d->owner = dueno;
 	return (d);
 }
