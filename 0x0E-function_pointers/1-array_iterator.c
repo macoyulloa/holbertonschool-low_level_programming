9ab3093f5dceedf0608447df96106e572d6ea580
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-13 17:48:25 +0000

    comprobation of the NULL

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -13,9 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t num;
 	int param = 0;
 
-	for (num = 0; num < size; num++)
-	{
-		param = array[num];
-		action(param);
-	}
+	if (action && size && array)
+		for (num = 0; num < size; num++)
+		{
+			param = array[num];
+			action(param);
+		}
 }
