b3c927622a08a589e1d884f88819285fd46d0f23
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-14 00:20:49 +0000

    code

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - description.
+ * @argc: input
+ * @argv: input
+ * Return: nothing.
+**/
+void main(int argc, char *argv[])
+{
+	int x = atoi(argv[1]);
+
+	if (x < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	else if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	printf("%0x\n", x);
+}
