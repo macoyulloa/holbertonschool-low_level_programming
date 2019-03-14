c0bc976d22b015fdfbe6eddbe12d54280afe4708
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-14 05:06:27 +0000

    code

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,9 +1,29 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
 /**
  * main - point 3.
- *
+ * @argc: input
+ * @argv: input
  * Return: Always 0.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
+	int prim = atoi(argv[2]);
+	int sec = atoi(argv[4]);
 
+	argc = 4;
+	if (argv[3] == '+')
+	{
+	}
+	else if (argv[3] == '-')
+	{
+	}
+	else if (argv[3] == '*')
+	{
+	}
+	else
+	{
+	}
+	return (0);
 }
