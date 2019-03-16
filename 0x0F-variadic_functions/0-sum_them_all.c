a6383aa57ad2143f02b997464b27c699c4477dc2
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-16 21:15:30 +0000

    code correction point 0

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_start(sum, n);
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 		i = i + va_arg(sum, int);
 	va_end(sum);
 	return (i);
