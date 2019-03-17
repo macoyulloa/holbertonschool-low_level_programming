a0c5f817142f8217a4042fa8a04f9e1a644ac3a6
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-17 22:43:19 +0000

    code correction

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - suma de los argumentos.
+ * @format: argumentos.
+ * Return: void.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list variable;
+	int i = 0;
+	char *p;
+
+	va_start(variable, format);
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+			{
+				case('c'):
+					printf("%c", va_arg(variable, int));
+					break;
+				case('i'):
+					printf("%i", va_arg(variable, int));
+					break;
+				case('f'):
+					printf("%f", va_arg(variable, double));
+					break;
+				case('s'):
+					p = va_arg(variable, char *);
+					if (p == NULL)
+					{
+						printf("(nil)");
+						break;
+					}
+					printf("%s", p);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			if (format[i + 1] != '\0')
+				printf(", ");
+			i++;
+		}
+	printf("\n");
+	va_end(variable);
+}
