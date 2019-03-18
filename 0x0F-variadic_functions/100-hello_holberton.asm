a8e7ac522604ce9e14677d8cc8df816a6bcfb1bb
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-18 00:42:51 +0000

    code correction advanced point

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,20 +1,17 @@
-; Define variables in the data section
 SECTION .DATA
 	hello:		db 'Hello, Holberton',10
 	helloLen:	equ $-hello
 
-; Code goes in the text section
 SECTION .TEXT
 	GLOBAL main
 
 main:
-	mov eax,4		; 'write' system call = 4
-	mov ebx,1		; file descriptor 1 = STDOUT
-	mov ecx,hello		; string to wirte
-	mov edx,helloLen	; length of string to write
-	int 80h			; call the kernel
+	mov eax,4
+	mov ebx,1
+	mov ecx,hello
+	mov edx,helloLen
+	int 80h
 
-	; Terminate program
-	mov eax,1		; 'exit' system call
-	mov ebx,0		; exit with error code 0
-	int 80h			; call the kernel
+	mov eax,1
+	mov ebx,0
+	int 80h
