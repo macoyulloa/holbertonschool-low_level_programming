1151d939a3a18a4b04f35c466f6d0096547c278b
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-21 22:26:36 +0000

    correction point 2

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -12,16 +12,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *c_str;
 	unsigned int new_len = 0;
 
-	c_str = strdup(str);
-	while (c_str[new_len] != '\0')
+	while (str[new_len] != '\0')
 		new_len++;
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = c_str;
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
