30c0d78cc8fb256912cdf0dc1a8b11429ce48b24
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-26 17:05:04 +0000

    output correction point 5

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -13,12 +13,11 @@ void free_listint2(listint_t **head)
 	listint_t *next_node;
 
 	if (*head == NULL)
-		*head = NULL;
+		return;
 	while (*head != NULL)
 	{
 		next_node = (*head)->next;
 		free(*head);
 		*head = next_node;
 	}
-	*head = NULL;
 }
