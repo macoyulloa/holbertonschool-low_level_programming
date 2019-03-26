7ea895aa4cf3c586f5a1595f8c7af95969f51ce7
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-26 20:11:07 +0000

    uploading code point 10

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,22 +3,6 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * len - return the leng of a linked list.
- * @h: linked list
- * Return: number of nodes.
- */
-unsigned int len(listint_t *h)
-{
-	unsigned int num_nodes = 0;
-
-	while (h != NULL)
-	{
-		h = h->next;
-		num_nodes++;
-	}
-	return (num_nodes);
-}
-/**
  * delete_nodeint_at_index - delete a n node.
  * @head: linked list
  * @index: the position of the node to be deleted
@@ -26,32 +10,24 @@ unsigned int len(listint_t *h)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp;
-	unsigned int leng;
+	listint_t *tmp, *link_next;
+	unsigned int i = 0;
 
-	leng = len(*head);
+	tmp = *head;
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
 	{
-		tmp = *head;
-		*head = (*head)->next;
+		*head = (tmp)->next;
 		free(tmp);
 		return (1);
 	}
-	/**else if (index == leng)
-	{
-	}**/
-	else if (index < leng)
-	{
-		while (index - 1 > 0)
-		{
-			*head = (*head)->next;
-			index--;
-		}
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
-		return (1);
-	}
-	else
+	for (; tmp != NULL && i < index - 1; i++)
+		tmp = tmp->next;
+	if (tmp == NULL || tmp->next == NULL)
 		return (-1);
+	link_next = tmp->next->next;
+	free(tmp->next);
+	tmp->next = link_next;
+	return (1);
 }
