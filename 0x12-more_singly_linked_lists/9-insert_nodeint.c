3612dcbb30f706f694a54b1ff1ef2a2dd6d0e852
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-26 20:45:54 +0000

    correction code 9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,15 +14,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *actual_node;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
 	new_node->n = n;
 	if (*head == NULL)
+	{
+		new_node->next = NULL;
 		return (new_node);
+	}
 	else if (idx == 0)
 	{
 		new_node->next = *head;
@@ -31,11 +33,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	actual_node = *head;
 	for (; actual_node != NULL && i < idx - 1; i++)
 		actual_node = actual_node->next;
-	if (actual_node == NULL || actual_node->next == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
+	if ((actual_node == NULL) || (actual_node->next == NULL))
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = actual_node->next;
 	actual_node->next = new_node;
 	return (new_node);
