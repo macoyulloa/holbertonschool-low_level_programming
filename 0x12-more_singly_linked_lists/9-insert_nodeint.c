76235d13a47f78c50dc5bf5634edb05721147209
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-26 20:24:45 +0000

    correction code 9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *actual_node;
 	unsigned int i = 0;
 
-	actual_node = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
@@ -29,6 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = *head;
 		return (new_node);
 	}
+	actual_node = *head;
 	for (; actual_node != NULL && i < idx - 1; i++)
 		actual_node = actual_node->next;
 	if (actual_node == NULL || actual_node->next == NULL)
