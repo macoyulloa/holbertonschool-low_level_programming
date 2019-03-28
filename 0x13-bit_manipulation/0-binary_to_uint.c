73e8b6d7ef22a721db3225fef94c3fa0d3e78643
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-28 20:37:38 +0000

    uploading code point 0

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * myAtoi - first point.
+ * @str: string.
+ * Return: unsigned int.
+ */
+unsigned int myAtoi(const char *str)
+{
+	unsigned int res = 0, i = 0;
+
+	for (; str[i] != '\0'; ++i)
+		res = (res * 10) + str[i] - '0';
+	return (res);
+}
+/**
+ * binary_to_uint - first point.
+ * @b: pointer to a string.
+ * Return: decimal.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = myAtoi(b);
+	unsigned int last_digit = 0;
+	unsigned int dec_value = 0, base = 1;
+
+	if (b == NULL)
+		return (0);
+	while (n)
+	{
+		last_digit = n % 10;
+		if (last_digit != 0 && last_digit != 1)
+			return (0);
+		n = n / 10;
+		dec_value += last_digit * base;
+		base = base * 2;
+	}
+	return (dec_value);
+}
---
+++ 0x13-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+0x13.c Bit manipulation: this directory has the excersise in order to learn about the bit manipulation.
