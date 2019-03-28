3d5a39201d3bc98a69275d4ff7362a257e68a714
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-28 22:25:32 +0000

    uploading code point 0

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * myAtoi - first point.
- * @str: string.
- * Return: unsigned int.
- */
-unsigned int myAtoi(const char *str)
-{
-	unsigned int res = 0, i = 0;
-
-	for (; str[i] != '\0'; ++i)
-		res = (res * 10) + str[i] - '0';
-	return (res);
-}
-/**
  * binary_to_uint - first point.
  * @b: pointer to a string.
  * Return: decimal.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int last_digit = 0, n;
-	unsigned int dec_value = 0, base = 1;
+	int n, i = 0;
+	unsigned int dec_value = 0;
 
 	if (b == NULL)
 		return (0);
-	n = myAtoi(b);
-	while (n)
-	{
-		last_digit = n % 10;
-		if (last_digit != 0 && last_digit != 1)
+	for (n = 0; b[n] != '\0'; n++)
+		if (b[n] != 48 && b[n] != 49)
 			return (0);
-		n = n / 10;
-		dec_value += last_digit * base;
-		base = base * 2;
+	while (b[i])
+	{
+		if (b[i] == 49)
+		{
+		dec_value = (dec_value << 1) | 1;
+		}
+		else if (b[i] == 48)
+		{
+		dec_value = (dec_value << 1);
+		}
+		i++;
 	}
 	return (dec_value);
 }
