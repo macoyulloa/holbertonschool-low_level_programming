5b995edbc85fbc0c15d01925c73b2996271fd0b9
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-04-02 23:50:43 +0000

    uploading point 1

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * create_file - read a file.
+ * @filename: name of the file
+ * @text_content: content to the file.
+ * Return: 1 on success.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, i;
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (text_content == NULL)
+		return (1);
+	if (file == -1 || filename == NULL)
+		return (-1);
+	for (i = 0; text_content[i]; i++)
+	;
+	write(file, text_content, i);
+	close(file);
+	return (1);
+}
