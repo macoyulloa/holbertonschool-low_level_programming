fd8ded0a764293d6e6e8f4811748186d67cf0553
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-04-03 00:44:02 +0000

    structure point advanced

---
+++ 0x14-file_io/100-elf_header.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - copies the content of a file to another file.
+ * @ac: number of arguments
+ * @av: array
+ * Return: success.
+ */
+int main(int ac, char **av)
+{
+	int file;
+
+	if (ac != 3)
+	{
+		file = open(av[1], O_RDONLY);
+		if (file == -1)
+			return (-1);
+	}
+	return (1);
+}
