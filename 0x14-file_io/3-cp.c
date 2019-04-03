186135cf6431712f4000b4cd105d08c1e3f4a052
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-04-03 03:01:23 +0000

    advanced point 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,14 +14,13 @@ int main(int ac, char **av)
 		file_from = open(av[1], O_RDONLY);
 		if (file_from == -1)
 		{
-			printf("Error: Can't read from file NAME_OF_THE_FILE");
+			write(file_from, "Error: Can't read from file NAME_OF_THE_FILE\n", 45);
 			exit(98);
 		}
 		file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 		if (file_to == -1)
 		{
-			printf("Error: Can't close fd FD_VALUE, followed
-by a new line, on the POSIX");
+			write(file_to, "Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX\n", 69);
 			exit(100);
 		}
 	}
