7afa7f9b9a28e453e50d936f0d567dc2820c82d6
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-04-03 00:57:58 +0000

    advanced point 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -7,12 +7,15 @@
  */
 int main(int ac, char **av)
 {
-	int file;
+	int file_from, file_to;
 
 	if (ac != 3)
 	{
-		file = open(av[1], O_RDONLY);
-		if (file == -1)
+		file_from = open(av[1], O_RDONLY);
+		if (file_from == -1)
+			return (-1);
+		file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		if (file_to == -1)
 			return (-1);
 	}
 	return (1);
