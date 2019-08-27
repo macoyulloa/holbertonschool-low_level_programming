b61c1b96d93256fe60ef12966655d3e908f2a47f
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-27 23:07:12 +0000

    directory

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+/**
+ * linear_search - return the first element
+ * @array: list of data
+ * @size: the size of the array
+ * @value: value to check
+ * Return: the first index where value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+  size_t i;
+
+  for(i = 0; i < size; i++)
+    {
+      printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+      if(array[i] == value)
+	return (i);
+    }
+  return (-1);
+}
